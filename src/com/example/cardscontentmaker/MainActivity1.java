09724b06de9fc3cfd87b6b632ad397dbfcf36f97
Sergey Baskov <sbaskov@lionstone.co.uk>
2015-01-27 11:31:12 +0100

    background images; async load

--- src/com/example/cardscontentmaker/MainActivity1.java
+++ src/com/example/cardscontentmaker/MainActivity1.java
@@ -1,19 +1,27 @@
 package com.example.cardscontentmaker;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Random;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.Menu;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.*;
 
 public class MainActivity1 extends Activity {
+
+	RelativeLayout rl;
 
 	public class Card{
 		public int W;
@@ -21,13 +29,6 @@ public class MainActivity1 extends Activity {
 		public double a;
 	}
 
-	RelativeLayout rl;
-	/*
-	ImageView tv;
-	ImageView upper = null;
-    ImageView lefter = null;
-    ImageView prev_tv = null;
-*/
 	TextView tv;
     TextView upper = null;
     TextView lefter = null;
@@ -37,7 +38,7 @@ public class MainActivity1 extends Activity {
 	{
 	    Random rnd = new Random(); 
 
-        float dpToPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 190, getResources().getDisplayMetrics());
+        float dpToPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, getResources().getDisplayMetrics());
         
 		int Wfixed = getResources().getDisplayMetrics().widthPixels;
         double sumA = 0;
@@ -55,6 +56,7 @@ public class MainActivity1 extends Activity {
         int col = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
 		float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 320, getResources().getDisplayMetrics());
 
+		new LoadBackground("http://icode.renren.com/getcode.do?t=web_reg&rnd=1357253201496", "androidfigure", tv).execute();
 		TextView newUpper = null;
 	    for(int i=from;i<to;i++)
 	    {
@@ -62,20 +64,18 @@ public class MainActivity1 extends Activity {
 	    	int newH = H;
 	    	
 	    	tv = new TextView(this);
-		    //tv.setBackgroundColor(Color.parseColor("#ff00ff"));
 		    col = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
 		    tv.setBackgroundColor(col);
-			/*
-			LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(newW, newH);
-			tv.setLayoutParams(layoutParams);
-			*/
-		//	tv.getLayoutParams().width = newW;
-		//	tv.getLayoutParams().height = newH;
 		    tv.setWidth(newW);
 		    tv.setHeight(newH);
 		    tv.setText(i+") " + cards[i].W + "x" + cards[i].H + "\n" + newW + "x" + newH);
 		  
-		    tv.setId(i+1);//>0
+		    if((new Random()).nextDouble()<.5)
+		    	tv.setBackgroundResource(R.drawable.ic_launcher);
+		    else
+		    	tv.setBackgroundResource(R.drawable.stub);
+
+		    tv.setId(i+1);
 
 		    RelativeLayout.LayoutParams rlp1 = new RelativeLayout.LayoutParams(
 	    		RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
@@ -111,6 +111,11 @@ public class MainActivity1 extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+		if (android.os.Build.VERSION.SDK_INT > 9) {
+			StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build(); 
+			StrictMode.setThreadPolicy(policy);
+		}
+		
 		ScrollView sv = new ScrollView(this);
 	    rl = new RelativeLayout(this);
 	    rl.setBackgroundColor(Color.parseColor("#111111"));
@@ -152,4 +157,49 @@ public class MainActivity1 extends Activity {
 		return true;
 	}
 
+	private class LoadBackground extends AsyncTask<String, Void, Drawable> {
+
+	    private String imageUrl , imageName;
+	    private TextView _tv;
+
+	    public LoadBackground(String url, String file_name, TextView tv) {
+	        this.imageUrl = url;
+	        this.imageName = file_name;
+	    }
+
+	    @Override
+	    protected void onPreExecute() {
+	        super.onPreExecute();
+	    }
+
+	    @Override
+	    protected Drawable doInBackground(String... urls) {
+
+	        try {
+	            InputStream is = (InputStream) this.fetch(this.imageUrl);
+	            Drawable d = Drawable.createFromStream(is, this.imageName);
+	            return d;
+	        } catch (MalformedURLException e) {
+	            e.printStackTrace();
+	            return null;
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	            return null;
+	        }
+	    }
+	    private Object fetch(String address) throws MalformedURLException,IOException {
+	        URL url = new URL(address);
+	        Object content = url.getContent();
+	        return content;
+	    }
+
+
+	    @Override
+	    protected void onPostExecute(Drawable result) {
+	        super.onPostExecute(result);
+	        rl.setBackgroundDrawable(result);
+	        //tv.setBackgroundDrawable(result);
+	    }
+	}
+	
 }
